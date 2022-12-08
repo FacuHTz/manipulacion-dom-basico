ff6191744d5091c97f00f43f06b9336901761f7a
Facundo Hetze <hetzefacundo@gmail.com>
2022-12-08 09:37:34 -0300

    agregar .gitignore y primeros archivos del proyecto

---
+++ main.js
@@ -0,0 +1,53 @@
+// console.log("Hello, World");
+
+// const h1 = document.querySelector("h1");
+// const p = document.querySelector("p");
+// const parrafito = document.querySelector(".parrafito");
+ //const pid = document.querySelector("#pid");
+// const input = document.querySelector("input");
+
+// console.log(input.value);//en consola
+
+// console.log({
+//     h1,
+//     p,
+//     parrafito,
+//     pid,
+//     input,
+// });
+
+
+//Escribiendo desde Javascript en el HTML
+
+// const h1 = document.querySelector("h1");
+
+// h1.innerHTML = "cambiamos el titulo con JavaScript"; //devuelve o establece la sintaxis HTML describiendo los descendientes del elemento.
+// h1.innerText = "cambiamos el titulo con JavaScript"; //representa el contenido de texto representado de un nodo y sus descendientes.
+// console.log(h1.getAttribute("pantalla"))
+// h1.setAttribute("pantalla, platziMotorola");
+
+// input.value = "456";
+
+// const img = document.createElement("img");
+
+// img.setAttribute("src","../Frontend Developer/img/10comun.png");
+
+// pid.append(img);
+
+//  Eventos en Javascript: interactuar con usuarios
+
+//con esta función concatenamos lo que está en los dos imputs cuando apretamos el botón calcuar
+// const input1 = document.getElementById("calculo1");
+// const input2 = document.getElementById("calculo2");
+// const calcular = document.querySelector(".btnCalcular");
+// const pResult = document.querySelector(".result");
+// const form = document.querySelector("#form");
+
+// function sumarInputValues(event) {
+//   console.log({event});
+//   event.preventDefault();
+//   const sumaInputs = +input1.value + +input2.value;
+//   pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+// btn.addEventListener("submit", sumarInputValues);
